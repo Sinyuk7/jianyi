655b445f1e3dc1e88501658fcbeb26a30f7cb14b
Sinyuk <80998062@qq.com>
2016-04-08 14:02:18 +0800

    Update offer presenter

--- app/src/main/java/com/sinyuk/jianyimaterial/feature/offer/OfferPresenterImpl.java
+++ app/src/main/java/com/sinyuk/jianyimaterial/feature/offer/OfferPresenterImpl.java
@@ -1,7 +1,18 @@
 package com.sinyuk.jianyimaterial.feature.offer;
 
+import com.sinyuk.jianyimaterial.mvp.BasePresenter;
+
 /**
  * Created by Sinyuk on 16.4.8.
  */
-public class OfferPresenterImpl {
+public class OfferPresenterImpl extends BasePresenter<OfferView> implements IOfferPresenter{
+    @Override
+    public int queryUserSchool() {
+        return 0;
+    }
+
+    @Override
+    public void updateUserSchool(int schoolIndex) {
+
+    }
 }
