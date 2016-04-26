8e9d0a7972d86a38055eedcc9d48840cf310faf3
Sinyuk <80998062@qq.com>
2016-04-26 23:16:35 +0800

    Refactor on comment

--- app/src/main/java/com/sinyuk/jianyimaterial/adapters/CommentsAdapter.java
+++ app/src/main/java/com/sinyuk/jianyimaterial/adapters/CommentsAdapter.java
@@ -3,12 +3,12 @@ package com.sinyuk.jianyimaterial.adapters;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.DrawableRequestBuilder;
@@ -19,7 +19,7 @@ import com.sinyuk.jianyimaterial.R;
 import com.sinyuk.jianyimaterial.glide.CropCircleTransformation;
 import com.sinyuk.jianyimaterial.utils.AnimUtils;
 import com.sinyuk.jianyimaterial.utils.FuzzyDateFormater;
-import com.sinyuk.jianyimaterial.utils.StringUtils;
+import com.sinyuk.jianyimaterial.utils.ScreenUtils;
 import com.sinyuk.jianyimaterial.widgets.MyCircleImageView;
 
 import java.util.Date;
@@ -35,11 +35,7 @@ import cimi.com.easeinterpolator.EaseSineOutInterpolator;
  */
 public class CommentsAdapter extends ExtendedRecyclerViewAdapter<String, CommentsAdapter.CommentItemViewHolder> {
 
-    private final DrawableRequestBuilder<String> avatarRequest;
-    private boolean enterAnimationLocked = false;
-    private boolean delayEnterAnimation = true;
-    private int lastAnimatedPosition = -1;
-
+    private static final float ANIM_SCALE_FACTOR = 1f;
     private final static String[] avatarUrls = new String[]{
             "http://ww2.sinaimg.cn/square/b29e155agw1eylork6523j20zk0npjvg.jpg",
             "http://ww4.sinaimg.cn/square/b29e155agw1eylorl860fj21hc0u078o.jpg",
@@ -54,16 +50,19 @@ public class CommentsAdapter extends ExtendedRecyclerViewAdapter<String, Comment
             "http://ww4.sinaimg.cn/mw690/b29e155agw1eyfew3f622j20e60amae5.jpg",
             "http://ww4.sinaimg.cn/square/b29e155agw1eyfew1ez6jj20dw0dwjuv.jpg"
     };
+    private final DrawableRequestBuilder<String> avatarRequest;
+    private boolean enterAnimationLocked = false;
+    private boolean delayEnterAnimation = true;
+    private int lastAnimatedPosition = -1;
 
     public CommentsAdapter(Context context) {
         super(context);
         avatarRequest = Glide.with(mContext).fromString()
-                .priority(Priority.HIGH)
+                .priority(Priority.IMMEDIATE)
                 .bitmapTransform(new CropCircleTransformation(mContext))
-                .crossFade()
+                .crossFade(1000)
                 .diskCacheStrategy(DiskCacheStrategy.RESULT)
-                .error(R.drawable.ic_avatar_placeholder)
-                .placeholder(R.drawable.ic_avatar_placeholder);
+                .error(R.drawable.ic_avatar_placeholder);
     }
 
     @Override
@@ -72,24 +71,26 @@ public class CommentsAdapter extends ExtendedRecyclerViewAdapter<String, Comment
     }
 
     private void runEnterAnimation(final View view, int position) {
-        if (enterAnimationLocked)
-            return;
+        if (enterAnimationLocked) { return; }
 
         if (position > lastAnimatedPosition) {
             lastAnimatedPosition = position;
-            view.setTranslationY(100);
+            view.setTranslationY(ScreenUtils.dpToPx(mContext, 56));
+            view.setPivotX(0);
+            view.setPivotY(0);
+            view.setRotation(30);
             view.setAlpha(0.f);
             view.animate()
-                    .translationY(0).alpha(1.f)
-                    .setStartDelay(delayEnterAnimation ? 30 * (position) : 0)
+                    .translationY(0).alpha(1.f).rotation(0)
+                    .setStartDelay(delayEnterAnimation ? (long) (ANIM_SCALE_FACTOR * 200 * position) : 0)
                     .setInterpolator(new EaseSineOutInterpolator())
-                    .setDuration(AnimUtils.ANIMATION_TIME_SHORT_EXTRA)
+                    .setDuration(AnimUtils.ANIMATION_TIME_MEDIUM)
                     .setListener(new AnimatorListenerAdapter() {
                         @Override
                         public void onAnimationEnd(Animator animation) {
-                            enterAnimationLocked = true;
                             ViewCompat.setTranslationY(view, 0);
                             ViewCompat.setAlpha(view, 1.f);
+                            ViewCompat.setRotation(view, 0);
                         }
                     })
                     .start();
@@ -105,21 +106,20 @@ public class CommentsAdapter extends ExtendedRecyclerViewAdapter<String, Comment
 
     @Override
     public void onBindDataItemViewHolder(CommentItemViewHolder holder, int position) {
-        runEnterAnimation(holder.itemView, position);
-        if (position % 2 == 0) {
-            holder.itemView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.grey_100));
-        } else {
-            holder.itemView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.grey_100));
-        }
+
+        enterAnimationLocked = getItemCount() <= position + 1;
 
         holder.userNameTv.setText(mContext.getResources().getStringArray(R.array.user_names)[position]);
 
         holder.pubDateTv.setText(FuzzyDateFormater.getTimeAgo(mContext, new Date(System.currentTimeMillis() - 6000000 * new Random().nextInt(100))));
 
-
         holder.contentTv.setMaxLines(new Random().nextInt(5) + 1);
 
         avatarRequest.load(avatarUrls[position]).into(holder.avatar);
+
+        runEnterAnimation(holder.itemView, position);
+
+
     }
 
 
@@ -140,6 +140,8 @@ public class CommentsAdapter extends ExtendedRecyclerViewAdapter<String, Comment
         TextView contentTv;
         @Bind(R.id.avatar)
         MyCircleImageView avatar;
+        @Bind(R.id.comment_view)
+        RelativeLayout commentView;
 
         public CommentItemViewHolder(View itemView) {
             super(itemView);
