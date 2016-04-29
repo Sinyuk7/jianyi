3542b4a8b52d1b333dc120200e98a119bfce397c
Sinyuk <80998062@qq.com>
2016-04-30 02:25:11 +0800

    I am in check null hell

--- app/src/main/java/com/sinyuk/jianyimaterial/feature/offer/OfferPresenterImpl.java
+++ app/src/main/java/com/sinyuk/jianyimaterial/feature/offer/OfferPresenterImpl.java
@@ -15,17 +15,20 @@ public class OfferPresenterImpl extends BasePresenter<OfferView> implements IOff
 
     @Override
     public void compressThenUpload(String str) {
+        if (mView == null) { return; }
         ShotModel.getInstance(mView).compressThenUpload(str, this);
     }
 
     @Override
-    public void post(@NonNull HashMap<String,String> urls,@NonNull String title, @NonNull String details, @NonNull String price, @NonNull String sort, String childSort) {
-        UserModel.getInstance(mView).postGoods(urls,title,details,price,sort,childSort,this);
+    public void post(@NonNull HashMap<String, String> urls, @NonNull String title, @NonNull String details, @NonNull String price, @NonNull String sort, String childSort) {
+        if (mView == null) { return; }
+        UserModel.getInstance(mView).postGoods(urls, title, details, price, sort, childSort, this);
         mView.showUploadProgress();
     }
 
     @Override
     public void onUploadParseError(String message) {
+        if (mView == null) { return; }
         mView.onShotUploadParseError(message);
     }
 
@@ -36,31 +39,37 @@ public class OfferPresenterImpl extends BasePresenter<OfferView> implements IOff
 
     @Override
     public void onUploadUploaded(String url) {
+        if (mView == null) { return; }
         mView.onShotUploadSucceed(url);
     }
 
     @Override
     public void onUploadCompressFailed(String message) {
+        if (mView == null) { return; }
         mView.onShotUploadCompressError(message);
     }
 
     @Override
     public void onPostGoodsSucceed(String message) {
+        if (mView == null) { return; }
         mView.onPostGoodsSucceed(message);
     }
 
     @Override
     public void onPostGoodsFailed(String message) {
+        if (mView == null) { return; }
         mView.onPostGoodsFailed(message);
     }
 
     @Override
     public void onPostGoodsVolleyError(String message) {
+        if (mView == null) { return; }
         mView.onPostGoodsVolleyError(message);
     }
 
     @Override
     public void onUPostGoodsParseError(String message) {
+        if (mView == null) { return; }
         mView.onUPostGoodsParseError(message);
     }
 }
