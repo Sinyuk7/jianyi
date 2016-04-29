8e99dea026350937e90bac8034524ec2b92a4c14
Sinyuk <80998062@qq.com>
2016-04-29 17:57:11 +0800

    Remove material intro

--- materialintro/src/main/java/co/mobiwise/materialintro/animation/MaterialIntroListener.java
+++
@@ -1,9 +0,0 @@
-package co.mobiwise.materialintro.animation;
-
-/**
- * Created by mertsimsek on 30/01/16.
- */
-public interface MaterialIntroListener {
-
-    void onUserClicked(String materialIntroViewId);
-}
