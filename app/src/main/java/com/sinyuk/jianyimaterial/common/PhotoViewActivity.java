d1fc71d56029c822e576903fa4f95e2a79fe33bf
Sinyuk <80998062@qq.com>
2016-05-15 02:11:13 +0800

    Update save button

--- app/src/main/java/com/sinyuk/jianyimaterial/common/PhotoViewActivity.java
+++ app/src/main/java/com/sinyuk/jianyimaterial/common/PhotoViewActivity.java
@@ -2,21 +2,19 @@ package com.sinyuk.jianyimaterial.common;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.RectF;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AlertDialog;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -31,13 +29,19 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringSystem;
 import com.sinyuk.jianyimaterial.R;
 import com.sinyuk.jianyimaterial.utils.FileUtils;
+import com.sinyuk.jianyimaterial.utils.LogUtils;
 import com.sinyuk.jianyimaterial.utils.ScreenUtils;
+import com.sinyuk.jianyimaterial.utils.SpringUtils;
 import com.sinyuk.jianyimaterial.utils.ToastUtils;
 import com.sinyuk.jianyimaterial.widgets.HackyViewPager;
 import com.sinyuk.jianyimaterial.widgets.InkPageIndicator;
 import com.sinyuk.jianyimaterial.widgets.RoundCornerIndicator;
+import com.tumblr.backboard.imitator.ToggleImitator;
+import com.tumblr.backboard.performer.MapPerformer;
 
 import java.util.ArrayList;
 
@@ -45,28 +49,21 @@ import butterknife.Bind;
 import butterknife.OnClick;
 import cimi.com.easeinterpolator.EaseSineInOutInterpolator;
 import uk.co.senab.photoview.PhotoView;
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class PhotoViewActivity extends BaseActivity {
 
     private static final String ISLOCKED_ARG = "is_locked";
     @Bind(R.id.view_pager)
     HackyViewPager viewPager;
-    @Bind(R.id.back_iv)
-    ImageView backIv;
     @Bind(R.id.page_indicator)
     InkPageIndicator pageIndicator;
     @Bind(R.id.page_indicator_compat)
     RoundCornerIndicator cornerIndicator;
-    @Bind(R.id.save_iv)
-    ImageView saveIv;
-    @Nullable
-    @Bind(R.id.root_view)
-    FrameLayout rootView;
     @Bind(R.id.progress_bar)
     ProgressBar progressBar;
-
-
+    @Bind(R.id.save_iv)
+    ImageView saveButton;
+    Handler myHandler = new Handler();
     private int selectedPageIndex;
     //    private String productId;
     private ArrayList<String> shotUrls;
@@ -98,8 +95,7 @@ public class PhotoViewActivity extends BaseActivity {
 
 
         Bundle bundle = getIntent().getExtras();
-        if (bundle == null)
-            return;
+        if (bundle == null) { return; }
         shotUrls = bundle.getStringArrayList("shot_urls");
         selectedPageIndex = bundle.getInt("selected_page_index", 0);
     }
@@ -125,17 +121,39 @@ public class PhotoViewActivity extends BaseActivity {
         progressBar.setIndeterminate(true);
         progressBar.setInterpolator(new EaseSineInOutInterpolator());
         setupViewPager();
+        setupSaveButton();
+    }
+
+    private void setupSaveButton() {
+
+        final SpringSystem springSystem = SpringSystem.create();
+
+        final Spring spring = springSystem.createSpring();
+
+        spring.addListener(new MapPerformer(saveButton, View.SCALE_X, 1, 1.2f));
+        spring.addListener(new MapPerformer(saveButton, View.SCALE_Y, 1, 1.2f));
+
+        saveButton.setOnTouchListener(new ToggleImitator(spring, 0, 1){
+            @Override
+            public boolean onTouch(View v, @NonNull MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    clickSaveButton();
+                    return true;
+                }
+                return super.onTouch(v, event);
+            }
+        });
+
+
     }
 
     private void setupViewPager() {
         // can't add this otherwise the photoView will not work
 //        viewPager.setPageTransformer(false, new DepthPageTransformer());
-        if (shotUrls.isEmpty())
-            return;
+        if (shotUrls.isEmpty()) { return; }
         viewPager.setAdapter(new PhotoViewAdapter(this));
 
-        if (shotUrls.size() == 1)
-            return;
+        if (shotUrls.size() == 1) { return; }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             pageIndicator.setViewPager(viewPager);
@@ -164,31 +182,28 @@ public class PhotoViewActivity extends BaseActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         if (isViewPagerActive()) {
-            outState.putBoolean(ISLOCKED_ARG, ((HackyViewPager) viewPager).isLocked());
+            outState.putBoolean(ISLOCKED_ARG, viewPager.isLocked());
         }
         super.onSaveInstanceState(outState);
     }
 
-    @OnClick({R.id.back_iv, R.id.save_iv})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.back_iv:
-                onBackPressed();
-                break;
-            case R.id.save_iv:
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            getString(R.string.permission_write_storage_rationale),
-                            REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
-                } else {
-                    downloadPic();
-                }
-                break;
+
+    private void clickSaveButton() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    getString(R.string.permission_write_storage_rationale),
+                    REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
+        } else {
+            downloadPic();
         }
+
+
     }
 
     private void downloadPic() {
+        
         // use application context for a persistent download
         downloadRequest.load(shotUrls.get(viewPager.getCurrentItem()))
                 .into(new SimpleTarget<Bitmap>() { // default means the original size
@@ -218,12 +233,9 @@ public class PhotoViewActivity extends BaseActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage(rationale)
                     .setCancelable(false)
-                    .setNegativeButton("取消", null).setPositiveButton("允许", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    ActivityCompat.requestPermissions(PhotoViewActivity.this,
-                            new String[]{permission}, requestCode);
-                }
+                    .setNegativeButton("取消", null).setPositiveButton("允许", (dialog, which) -> {
+                ActivityCompat.requestPermissions(PhotoViewActivity.this,
+                        new String[]{permission}, requestCode);
             }).setTitle(getResources().getString(R.string.permission_title_rationale));
 
             AlertDialog alertDialog = builder.show();
@@ -263,7 +275,11 @@ public class PhotoViewActivity extends BaseActivity {
         return (viewPager != null);
     }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myHandler.removeCallbacksAndMessages(null);
+    }
 
     class PhotoViewAdapter extends PagerAdapter {
 
@@ -279,17 +295,11 @@ public class PhotoViewActivity extends BaseActivity {
 
         @Override
         public View instantiateItem(final ViewGroup container, int position) {
-            if (null != progressBar)
-                progressBar.setVisibility(View.VISIBLE);
+            if (null != progressBar) { progressBar.setVisibility(View.VISIBLE); }
 
             final PhotoView photoView = new PhotoView(container.getContext());
 
-            photoView.setOnMatrixChangeListener(new PhotoViewAttacher.OnMatrixChangedListener() {
-                @Override
-                public void onMatrixChanged(RectF rect) {
-                    viewPager.setLocked(photoView.getScale() != 1);
-                }
-            });
+            photoView.setOnMatrixChangeListener(rect -> viewPager.setLocked(photoView.getScale() != 1));
 
             container.addView(photoView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
@@ -297,16 +307,19 @@ public class PhotoViewActivity extends BaseActivity {
             displayRequest.load(shotUrls.get(position)).listener(new RequestListener<String, GlideDrawable>() {
                 @Override
                 public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
-                    if (null != progressBar)
-                        progressBar.setVisibility(View.GONE);
+                    if (null != progressBar) { progressBar.setVisibility(View.GONE); }
                     ToastUtils.toastSlow(PhotoViewActivity.this, "加载失败");
                     return false;
                 }
 
                 @Override
                 public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
-                    if (null != progressBar)
+                    if (null != progressBar) {
                         progressBar.setVisibility(View.GONE);
+                    }
+
+                    myHandler.postDelayed(() -> SpringUtils.popOut(saveButton, 100, 10), 1000);
+
                     return false;
                 }
             }).into(photoView);
