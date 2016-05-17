d51bfd16891e19a0f26dc0ac463490daaf016027
Sinyuk <80998062@qq.com>
2016-05-17 13:59:08 +0800

    Refactor on category view search widget

--- app/src/main/java/com/sinyuk/jianyimaterial/feature/CategoryView.java
+++ app/src/main/java/com/sinyuk/jianyimaterial/feature/CategoryView.java
@@ -1,26 +1,21 @@
 package com.sinyuk.jianyimaterial.feature;
 
-import android.app.SearchManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.view.Menu;
 import android.view.View;
 
 import com.sinyuk.jianyimaterial.R;
-import com.sinyuk.jianyimaterial.common.SearchActivity;
 import com.sinyuk.jianyimaterial.adapters.CategoryMenuAdapter;
 import com.sinyuk.jianyimaterial.common.BaseActivity;
 import com.sinyuk.jianyimaterial.feature.explore.ExploreView;
-import com.sinyuk.jianyimaterial.utils.ImeUtils;
+import com.sinyuk.jianyimaterial.feature.search.SearchingView;
 import com.sinyuk.jianyimaterial.utils.LogUtils;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 public class CategoryView extends BaseActivity implements CategoryMenuAdapter.OnCategoryMenuItemClickListener {
 
@@ -83,32 +78,9 @@ public class CategoryView extends BaseActivity implements CategoryMenuAdapter.On
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.action_bar_search, menu);
-        // Get the SearchingView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-
-        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchActivity.class)));
-        searchView.setQueryRefinementEnabled(true); //Query refinement for search suggestions
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                // 得到输入管理对象
-                ImeUtils.hideIme(searchView);
-                searchView.setIconified(true);
-                searchView.clearFocus();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        return true;
+    @OnClick(R.id.search_button)
+    public void toSearchView() {
+        startActivity(new Intent(CategoryView.this, SearchingView.class));
     }
 
 }
